dea41a4468ac119e31f0c571c55a80b6504de373
Hugo da Silva e Souza <hugo.souza@estudante.ufscar.br>
2022-06-22 11:17:03 -0300

    Add new methods

--- SPI_Communication_Service.cpp.cpp
+++ SPI_Communication_Service.cpp.cpp
@@ -2,26 +2,60 @@
 
 
 // Default construction
-SPI_Communication::SPI_Communication(int channel, int operationMode, int communicationSpeed, int bitsPerWord)
+SPI_Communication::SPI_Communication(unsigned int channel, unsigned int device, int operationMode, uint8_t bitsPerWord, uint32_t communicationSpeed, uint16_t communicationDelay)
 {
-    
+    stringstream s;
+    s << SPI_PATH << channel << "." << device;
+    this->fileName = string(s.str());
+	this->operationMode = operationMode;
+	this->bitsPerWord = bitsPerWord;
+	this->communicationSpeed = communicationSpeed;
+	this->communicationDelay = communicationDelay;
+	this->spi_channel_communication();
 }
 
 // Default destructor
 SPI_Communication::~SPI_Communication()
 {
-
+    this->close_spi_communication();
 }
+
 
 /******************* spi_channel_configuration **************************
 *************************************************************************
 Operation: Configure the SPI channel according to the parameters;
 Input Parameters: Nothing;
-Output: Status of configuration;
+Output: Status of configuration.
 */
-void SPI_Communication::spi_channel_configuration()
+int SPI_Communication::spi_channel_configuration()
 {
-    
+    int error;
+
+    this->fileDescriptor = ::open(this->fileName.c_str(), O_RDWR);
+    if(this->fileDescriptor < 0){
+        perror("SPI: Can't open device.");
+        return -1;
+    }
+	
+    error = this->set_operation_mode(this->operationMode);
+    if(error < 0){
+        perror("SPI: Can't set operation mode.");
+        return -1;
+    }
+
+    error = this->set_communication_speed(this->communicationSpeed);
+    if(error < 0){
+        perror("SPI: Can't set communication speed.");
+        return -1;
+    }
+
+    error = this->set_bits_per_word(this->bitsPerWord);
+    if(error < 0){
+        perror("SPI: Can't set bits per word.");
+        return -1;
+    }
+
+	return 0;
 }
 
 /******************* enable_device_communication **************************
@@ -113,3 +147,68 @@ void SPI_Communication::read_device(int requisitionCode, int readLength)
         abort();
     }
 }
+
+/*
+Falta colocar os cabeçalhos das funções abaixo
+*/
+int SPI_Communication::set_operation_mode(int operationMode)
+{
+    this->mode = operationMode;
+    error = octl(this->fileName, SPI_IOC_WR_MODE, &this->operationMode);
+	if (error < 0){
+		perror("SPI: Can't set SPI mode.");
+		return -1;
+	}
+
+    error = ioctl(this->fileName, SPI_IOC_RD_MODE, &this->operationMode);
+	if (error < 0){
+		perror("SPI: Can't get SPI mode.");
+		return -1;
+	}
+
+	return 0;
+}
+
+int SPI_Communication::set_communication_speed(uint32_t communicationSpeed)
+{
+    this->communicationSpeed = communicationSpeed;
+
+    error = ioctl(this->fileName, SPI_IOC_WR_MAX_SPEED_HZ, &this->communicationSpeed);
+	if(error < 0){
+		perror("SPI: Can't set max speed HZ");
+		return -1;
+	}
+
+    error = ioctl(this->fileName, SPI_IOC_RD_MAX_SPEED_HZ, &this->communicationSpeed);
+	if(error < 0){
+		perror("SPI: Can't get max speed HZ.");
+		return -1;
+	}
+
+	return 0;
+}
+
+int SPI_Communication::set_bits_per_word(uint8_t bitsPerWord)
+{
+    this->bitsPerWord = bitsPerWord;
+
+    error = ioctl(this->fileName, SPI_IOC_WR_BITS_PER_WORD, &this->bitsPerWord);
+	if(error < 0){
+		perror("SPI: Can't set bits per word.");
+		return -1;
+	}
+
+    error = ioctl(this->file, SPI_IOC_RD_BITS_PER_WORD, &this->bitsPerWord);
+	if(error < 0){
+		perror("SPI: Can't get bits per word.");
+		return -1;
+	}
+	return 0
+}
+
+void SPI_Communication::close_spi_communication()
+{
+    ::close(this->fileName);
+    this->fileName = -1;
+}
+
--- SPI_Communication_Service.h
+++ SPI_Communication_Service.h
@@ -14,17 +14,21 @@
 #include <wiringPi.h>
 
 
+#define SPI_PATH "/dev/spidev"
+using namespace std;
+
+
 class SPI_Communication
 {
 public:
     // Constructor
-    SPI_Communication();
+    SPI_Communication(unsigned int channel, unsigned int device, int operationMode, uint8_t bitsPerWord, uint32_t communicationSpeed, uint16_t communicationDelay);
 
     // Destructor
     ~SPI_Communication();
 
     // Configure the SPI channel according to the parameters
-    void spi_channel_configuration(int channel, int operationMode, int communicationSpeed, int bitsPerWord);
+    void spi_channel_configuration();
 
     // Select the device to enable communication
     void enable_device_communication(int devicePinSelector);
@@ -33,12 +37,19 @@ public:
     void disable_device_communication(int devicePinSelector);
 
     // Select RaspberryPi's SPI channel and send the menssage
-    void write_device(int channel, int communicationDelay, int communicationSpeed, int bitsPerWord, int data);
+    void write_device();
 
     // Select RaspberryPi's SPI channel and read data from device
-    void read_device(int channel, int communicationDelay, int communicationSpeed, int bitsPerWord, int requisitionCode);
+    void read_device();
 private:
-    int descriptor;
+    unsigned int channel;
+    unsigned int device;
+    int fileDescriptor;
+    std::string fileName;
+    uint8_t bitsPerWord;
+    uint32_t communicationSpeed
+    uint16_t communicationDelay;
+    int operationMode;
 };
 
 #endif
